ab77e74fd437f067c6223d2144fc1b686c4c222a
amin <amin.roosta@outlook.com>
2021-06-24 09:39:55 +0200

    .

--- say.js
+++ say.js
@@ -3,28 +3,48 @@ const fs = require('fs');
 const tts = require('google-translate-tts');
 const translate = require('@vitalets/google-translate-api');
 const shell = require('shelljs');
-var readline = require('readline');
-var rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-      terminal: false
-});
+const child_process = require('child_process');
 
+
+
+// let previousWord = '';
 function online(text) {
-	translate(text, {to: 'en', from: 'de'}).then(eng => {
+	return translate(text, {to: 'en', from: 'de'}).then(eng => {
         if(eng.from.text.value) {
             console.log("\x1b[36m%s\x1b[0m", eng.from.text.value);
             return online(eng.from.text.value.replace('[', '').replace(']', ''));
         }
-        console.log("\x1b[36m%s\x1b[0m", eng.text);
+        child_process.fork(__filename, [text]);
+        // setTimeout(() => {
 
-        translate(text, {to: 'fa', from: 'de'}).then(farsi => console.log("\x1b[36m%s\x1b[0m", farsi.text));
-        tts.synthesize({ text, voice: 'de', slow: false }).then(buffer => {
-            fs.writeFileSync('/tmp/voice.mp3', buffer);
-            shell.exec('mpg123 /tmp/voice.mp3 > /dev/null 2>&1');
-            shell.exec('afplay /tmp/voice.mp3 > /dev/null 2>&1');
-        });
+        //     const slow = text == previousWord;
+        //     previousWord = text;
+        //     tts.synthesize({ text, voice: 'de', slow: slow }).then(buffer => {
+        //         fs.writeFileSync('/tmp/voice.mp3', buffer);
+        //         shell.exec('mpg123 /tmp/voice.mp3 > /dev/null 2>&1');
+        //         shell.exec('afplay /tmp/voice.mp3 > /dev/null 2>&1');
+        //     });
+        // }, 0);
+        return eng.text;
     });
 }
 
-rl.on('line', online);
+if(process.argv[2]) {
+    tts.synthesize({ text: process.argv[2], voice: 'de', slow: false }).then(buffer => {
+        fs.writeFileSync('/tmp/voice.mp3', buffer);
+        shell.exec('mpg123 /tmp/voice.mp3 > /dev/null 2>&1');
+        shell.exec('afplay /tmp/voice.mp3 > /dev/null 2>&1');
+    });
+}
+else {
+    var repl = require("repl");
+    function myEval(cmd, context, filename, callback) {
+        cmd = cmd.trim();
+        if(!cmd) {
+            callback(null, '');
+        } else {
+            online(cmd).then(t => callback(null, t));
+        }
+    }
+    repl.start({ prompt: '> ', eval: myEval });
+}
